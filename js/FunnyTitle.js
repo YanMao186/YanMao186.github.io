36a772f094424fec72870556f7995e7f8b0ba95e
YanMao186 <18629281909@163.com>
2019-07-20 21:18:08 +0800

    Site updated: 2019-07-20 21:18:06

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -4,12 +4,12 @@
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
          $('[rel="icon"]').attr('href', "/img/trhx2.png");
-         document.title = 'ヽ(●-`Д´-)ノ你丑你就走！';
+         document.title = '客官来呀！';
          clearTimeout(titleTime);
      }
      else {
          $('[rel="icon"]').attr('href', "/img/trhx2.png");
-         document.title = 'ヾ(Ő∀Ő3)ノ你帅就回来！' + OriginTitle;
+         document.title = '客官里面请！' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
          }, 2000);
