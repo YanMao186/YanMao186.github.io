e14280a65489ecb6cff528b883ae09ee9ca038fa
YanMao186 <18629281909@163.com>
2019-11-13 11:03:45 +0800

    Site updated: 2019-11-13 11:03:43

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -4,12 +4,12 @@
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
          $('[rel="icon"]').attr('href', "/img/trhx2.png");
-         document.title = '客官来呀！';
+         document.title = '....！';
          clearTimeout(titleTime);
      }
      else {
          $('[rel="icon"]').attr('href', "/img/trhx2.png");
-         document.title = '客官里面请！' + OriginTitle;
+         document.title = '....！' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
          }, 2000);
